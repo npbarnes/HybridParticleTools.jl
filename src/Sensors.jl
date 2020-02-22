1ee1a5d5625c0c5bdf4ea1a19af4de09d580a4b2
Nathan Barnes <npbarnes@alaska.edu>
2020-02-21 16:52:20 -0900

    Change handling of empty distributions

--- src/Sensors.jl
+++ src/Sensors.jl
@@ -25,20 +25,18 @@ thermalenergy(f::Distribution) = pressure(f)/density(f)
 
 "Compute E/q spectrum for the given distribution"
 function energyspectrogram(f::Distribution) where T
-    if length(f.ms) == 0
-        return Histogram
-    end
     Eoverq = (1/2 .* f.ms .* norm.(f.vs).^2) ./ f.qs
     Eoverq_ = ustrip.(u"J*C^-1", Eoverq)
-    G = 0.1u"cm^2*sr" * 10u"s" / (4π*u"sr") # hypothetical 4π sr particle detector
+    G = 0.15u"cm^2*sr" * 10u"s" / (4π*u"sr") # hypothetical 4π sr particle detector
     counts = ustrip.(Unitful.NoUnits, G .* norm.(f.vs) .* f.ns)
     fit(Histogram, Eoverq_, weights(log10.(counts)), exp10.(range(1, stop=4, length=64)))
 end
 
-flythrough(s::Simulation, traj::Trajectory, sensor) = flythrough(s, traj.pos, sensor)
-function flythrough(s::Simulation, traj_arr::AbstractVector, sensor)
+flythrough(s::Simulation, traj::Trajectory, sensor, sensor_args...) = flythrough(s, traj.pos, sensor, sensor_args...)
+function flythrough(s::Simulation, traj_arr::AbstractVector, sensor, sensor_args...)
     ds = Distribution.(s, traj_arr)
-    sensor.( filter(x->length(x.ms)!=0, ds) )
+    T = typeof(sensor(ds[1],sensor_args...))
+    Vector{Union{T,Nothing}}[length(d)==0 ? nothing : sensor(d,sensor_args...) for d in ds]
 end
 
 end # module
