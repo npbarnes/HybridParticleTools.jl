3585c070a822e74b4621fa4ddd56a519ba8180b5
Nathan Barnes <npbarnes@alaska.edu>
2020-04-27 13:52:08 -0800

    Include PlutoUnits and PlottingTools

--- src/HybridParticleTools.jl
+++ src/HybridParticleTools.jl
@@ -1,7 +1,13 @@
 module HybridParticleTools
 export  Trajectory, Parameters, Simulation, touching, Distribution,
         density, bulkvelocity, pressuretensor, pressure, thermalenergy,
-        energyspectrogram, flythrough
+        energyspectrogram, flythrough,
+        Tag, dummy, H_sw, He_sw, H_ipui, He_ipui, CH4_photo, CH4_stagnant, CH4_chex,
+        fov_polygon
+
+include("PlutoUnits")
+import Unitful
+Unitful.register(PlutoUnits)
 
 include("Utility.jl")
 include("SphericalShapes.jl")
@@ -10,11 +16,11 @@ include("Simulations.jl")
 include("Distributions.jl")
 include("Spacecraft.jl")
 include("Sensors.jl")
+include("PlottingTools.jl")
 
 using .Simulations
 using .Distributions
 using .Spacecraft
 using .Sensors
-
 
 end # module
