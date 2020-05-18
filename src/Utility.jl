734ae5688b508bbe5eb5416f7461729e97520bba
Nathan Barnes <npbarnes@alaska.edu>
2020-05-18 11:09:21 -0800

    Update documentation

--- src/Utility.jl
+++ src/Utility.jl
@@ -12,7 +12,7 @@ A copy is made.
 function listofvectors(arr::AbstractArray{T,2}) where T
     SVector{3,T}.(eachrow(arr))
 end
-"Convert Array{T,3} to Vector{SMatrix{3,3,T}}"
+"Convert Array{T,3} with size (3,3,:) to Vector{SMatrix{3,3,T}}"
 function listofmatrices(arr::AbstractArray{T,3}) where T
     SMatrix{3,3,T,9}.(eachslice(arr,dims=1))
 end
@@ -21,6 +21,7 @@ function geomspace(start, stop, N)
     exp10.(range(log10(start), stop=log10(stop), length=N))
 end
 
+"No-copy ustrip for Arrays of StaticArrays of Quantities"
 asunitless(A::AbstractArray{SA}) where {Size, Q<:Quantity, SA<:StaticArray{Size, Q}} = reinterpret(similar_type(SA,Unitful.numtype(Q)), A)
 
 end # module
