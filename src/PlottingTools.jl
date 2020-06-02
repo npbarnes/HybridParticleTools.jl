0822c67bd3ccfa35bbc229eef7e990eff2bf2fe2
Nathan Barnes <npbarnes@alaska.edu>
2020-06-02 14:22:32 -0800

    Make coloredline behave more like other matplotlib plotting routines.
    Also fix indentation.

--- src/PlottingTools.jl
+++ src/PlottingTools.jl
@@ -16,69 +16,77 @@ const vec_coords = PyNULL()
 const geodetic = PyNULL()
 
 function __init__()
-   copy!(plt, pyimport("matplotlib.pyplot"))
-   copy!(Axes3D, pyimport("mpl_toolkits.mplot3d").Axes3D)
-   copy!(ccrs, pyimport("cartopy.crs"))
-   copy!(unit_sphere, ccrs.Globe(semimajor_axis=1., semiminor_axis=1., ellipse=nothing))
-   copy!(map_projection, ccrs.Mollweide(globe=unit_sphere))
-   copy!(vec_coords, map_projection.as_geocentric())
-   copy!(geodetic, map_projection.as_geodetic())
+    copy!(plt, pyimport("matplotlib.pyplot"))
+    copy!(Axes3D, pyimport("mpl_toolkits.mplot3d").Axes3D)
+    copy!(ccrs, pyimport("cartopy.crs"))
+    copy!(unit_sphere, ccrs.Globe(semimajor_axis=1., semiminor_axis=1., ellipse=nothing))
+    copy!(map_projection, ccrs.Mollweide(globe=unit_sphere))
+    copy!(vec_coords, map_projection.as_geocentric())
+    copy!(geodetic, map_projection.as_geodetic())
 
-   py"""
-   import numpy as np
-   def plot_sphere(ax, r, x_offset=0, **kwargs):
-      u=np.linspace(0,2*np.pi,100)
-      v=np.linspace(0,np.pi,100)
-      x = r * np.outer(np.cos(u), np.sin(v)) - x_offset
-      y = r * np.outer(np.sin(u), np.sin(v))
-      z = r * np.outer(np.ones(np.size(u)), np.cos(v))
-      return ax.plot_surface(x,y,z,**kwargs)
-   """
+    py"""
+    import numpy as np
+    def plot_sphere(ax, r, x_offset=0, **kwargs):
+        u=np.linspace(0,2*np.pi,100)
+        v=np.linspace(0,np.pi,100)
+        x = r * np.outer(np.cos(u), np.sin(v)) - x_offset
+        y = r * np.outer(np.sin(u), np.sin(v))
+        z = r * np.outer(np.ones(np.size(u)), np.cos(v))
+        return ax.plot_surface(x,y,z,**kwargs)
+    """
 
-   py"""
-   import numpy as np
-   import matplotlib.pyplot as plt
-   from matplotlib.collections import LineCollection
-   from mpl_toolkits.mplot3d import Axes3D
-   from mpl_toolkits.mplot3d.art3d import Line3DCollection
+    py"""
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from matplotlib.collections import LineCollection
+    from mpl_toolkits.mplot3d import Axes3D
+    from mpl_toolkits.mplot3d.art3d import Line3DCollection
 
-   def _as_segments(coords):
-       points = np.asanyarray(coords).T.reshape(-1,1,len(coords))
-       return np.concatenate([points[:-1], points[1:]], axis=1)
+    def _as_segments(coords):
+        points = np.asanyarray(coords).T.reshape(-1,1,len(coords))
+        return np.concatenate([points[:-1], points[1:]], axis=1)
 
-   def coloredline(ax, *args, **kwargs):
-       # coloredline(ax, x, y, [z], c, **kwargs)
-       # plot a line in ax with points x,y,z
-       # c is an array of scalars that are mapped to colors using the usual norm/cmap mechanism.
-       # kwargs are any matplotlib.collections.Collection kwargs e.g.
-       # norm, cmap, linewidths. See matplotlib documentation for the full list.
-       if len(args) != 3 && len(args) != 4:
-           raise TypeError(f"coloredline() takes 4 or 5 positional arguments, but {len(args)+1} were given")
-       coords = args[:-1]
-       c = args[-1]
-       segments = _as_segments(coords)
-       if len(coords) == 2: # 2d
-           lc = LineCollection(segments, **kwargs)
-       else: # 3d
-           lc = Line3DCollection(segments, **kwargs)
+    def coloredline(ax, *args, **kwargs):
+        # coloredline(ax, x, y, [z], c, **kwargs)
+        # plot a line in ax with points x,y,z
+        # c is an array of scalars that are mapped to colors using the usual norm/cmap mechanism.
+        # kwargs are any matplotlib.collections.Collection kwargs e.g.
+        # norm, cmap, linewidths. See matplotlib documentation for the full list.
+        if len(args) != 3 and len(args) != 4:
+            raise TypeError(f"coloredline() takes 4 or 5 positional arguments, but {len(args)+1} were given")
+        coords = args[:-1]
+        c = args[-1]
+        segments = _as_segments(coords)
+        if len(coords) == 2: # 2d
+            lc = LineCollection(segments, **kwargs)
+        else: # 3d
+            lc = Line3DCollection(segments, **kwargs)
 
-       lc.set_array(c)
-       lc.autoscale()
-       line = ax.add_collection(lc)
-       return line
-   """
-end
+        # Set the scalar array that is to be mapped to colors
+        lc.set_array(c)
+
+        # Scale limits of the norm if they aren't set already
+        lc.autoscale_None()
+
+        # Add the collection to the axes, and recalculating data limits
+        line = ax.add_collection(lc, autolim=True)
+
+        # Set axis limits to the data limits unless set explicitly with e.g. ax.set_xlim()
+        ax.autoscale_view()
+        return line
+    """
+end # __init__
 
 plot_sphere(ax, r, x_offset=0; kwargs...) = py"plot_sphere"(ax, r, xoffset, kwargs...)
 plot_colored_line(ax, args...; kwargs...) = py"coloredline"(ax, args...; kwargs...)
 
 function mapcoords(vs; from_crs=vec_coords, to_crs=map_projection)
-   mc = to_crs.transform_points(from_crs,
-      x=getindex.(vs,1),
-      y=getindex.(vs,2),
-      z=getindex.(vs,3)
-   )
-   @views (mc[:,1], mc[:,2])
+    mc = to_crs.transform_points(from_crs,
+        x=getindex.(vs,1),
+        y=getindex.(vs,2),
+        z=getindex.(vs,3)
+    )
+    @views (mc[:,1], mc[:,2])
 end
 
 function plot_3d_dist(ax, d)
@@ -90,18 +98,18 @@ function plot_3d_dist(ax, d)
 end
 
 function plotshape(ax, sp; kwargs...)
-   es = collect(Iterators.flatten(edges(sp)))
-   ax.plot(mapcoords(es, to_crs=geodetic)...; transform=geodetic, kwargs...)
+    es = collect(Iterators.flatten(edges(sp)))
+    ax.plot(mapcoords(es, to_crs=geodetic)...; transform=geodetic, kwargs...)
 end
 
 function circlepoints(a,N=100)
-   [(cos(a), sin(a)*sin(t), -sin(a)*cos(t)) for t in 2π/N*(0:N)]
+    [(cos(a), sin(a)*sin(t), -sin(a)*cos(t)) for t in 2π/N*(0:N)]
 end
 
 function circlegrid(ax, N=5)
-   for a in π/(N+1)*(1:N)
-      ax.plot(mapcoords(circlepoints(a), to_crs=geodetic)..., transform=geodetic, color="gray", linewidth=0.5, alpha=0.75)
-   end
+    for a in π/(N+1)*(1:N)
+        ax.plot(mapcoords(circlepoints(a), to_crs=geodetic)..., transform=geodetic, color="gray", linewidth=0.5, alpha=0.75)
+    end
 end
 
 function mapfigure()
@@ -122,11 +130,11 @@ function plot_pepssi(ax, et)
 end
 
 function plot_dist(fig, ax, d; marker=".", s=30.0, kwargs...)
-   l = -asunitless(d.v)
-   E = ustrip(uconvert.(u"keV", energy.(d)))
-   mappable = ax.scatter(mapcoords(l)...; marker=marker, s=s, c=E, kwargs...)
-   cb = fig.colorbar(mappable)
-   cb.set_label("Energy (keV)")
+    l = -asunitless(d.v)
+    E = ustrip(uconvert.(u"keV", energy.(d)))
+    mappable = ax.scatter(mapcoords(l)...; marker=marker, s=s, c=E, kwargs...)
+    cb = fig.colorbar(mappable)
+    cb.set_label("Energy (keV)")
 end
 plot_sun(ax) = ax.scatter(mapcoords([[1.,0.,0.]])..., marker="*", edgecolors="k", color="gold", s=200)
 plot_pluto(ax,pos::AbstractArray) = ax.scatter(mapcoords([-ustrip(pos)])..., marker=raw"$♥$",  edgecolors="k", color="chocolate", s=100)
@@ -144,7 +152,7 @@ end
 
 """
 Expand x and concat y so that they may be used for a scatter plot. That is,
-each y needs to be concatinated and x needs to be expanded by duplicating its
+each y needs to be concatenated and x needs to be expanded by duplicating its
 entries.
 
 Examples:
