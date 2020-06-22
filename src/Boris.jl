a761d0610f7c043f5dae35b02f62ccd50559c41c
Nathan Barnes <npbarnes@alaska.edu>
2020-06-22 13:33:13 -0800

    Reduce this down to functions I use and want to support

--- src/Boris.jl
+++ src/Boris.jl
@@ -6,9 +6,6 @@ using LinearAlgebra
 using ..Distributions
 import PhysicalConstants.CODATA2018: m_p, e
 
-function vstep!(v,dt,E,B)
-    v .= vstep(v,dt,E,B)
-end
 function vstep(v,dt,E,B)
     t = B*dt/2
     s = 2t/(1+t⋅t)
@@ -18,20 +15,12 @@ function vstep(v,dt,E,B)
     v⁺ = v⁻ + v′ × s
     return v⁺ + E*dt/2
 end
-function xstep!(x,v)
-    x .= xstep(x,v)
-end
+
 function xstep(x,dt,v)
     x + v*dt
 end
 
-function borisstep(x, v, dt, E, B)
-    v = next_v(v,dt,E,B)
-    x = next_x(x,v)
-    return x,v
-end
-
-function boris(xinit, vinit, dt, E, B, N;m=m_p,q=e)
+function boris(xinit, vinit, dt, E, B, N, m=m_p, q=e)
     ret_x = Vector{typeof(xinit)}(undef, N+1)
     ret_v = Vector{typeof(vinit)}(undef, N+1)
     ret_x[1] = xinit
@@ -44,16 +33,6 @@ function boris(xinit, vinit, dt, E, B, N;m=m_p,q=e)
         ret_x[i+1] = xstep(ret_x[i], dt, ret_v[i+1])
     end
     return ret_x, ret_v
-end
-
-function boris(d::Distribution, xinit, dt, E, B, N)
-    broadcast((x,v,dt,E,B,N,m,q) -> boris(x,v,dt,E,B,N,m=m,q=q),
-        Ref(xinit),
-        d.v,
-        dt,
-        (x,y,z)->E(x,y,z),
-        (x,y,z)->B(x,y,z),
-        N, d.m, d.q)
 end
 
 end # module
