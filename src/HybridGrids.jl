7d9bc46d98b1afd88daf6a53154c3e77ef78f020
Nathan Barnes <npbarnes@alaska.edu>
2020-08-19 14:31:07 -0800

    Add support for Yee Grid interpolation

--- src/HybridGrids.jl
+++ src/HybridGrids.jl
@@ -18,6 +18,71 @@ function __init__()
     copy!(hr,pyimport("HybridReader2").HybridReader2)
 end
 
+struct Grid{T}
+    nodes::NTuple{3, Vector{T}}
+end
+Grid(x,y,z) = Grid((x,y,z))
+struct YeeGrid{T}
+    maingrid::Grid{T}
+    dualgrid::Grid{T}
+    function YeeGrid(maingrid::Grid{T}) where T
+        new{T}(maingrid, _dualgrid(maingrid))
+    end
+end
+function _ave(q)
+    ret = similar(q)
+    ret[1:end-1] = 0.5*(q[1:end-1] + q[1+1:end])
+    ret[end] = q[end] + (q[end] - ret[end-1])
+    return ret
+end
+function _dualgrid(g::Grid)
+    (_ave(g.nodes[1]), _ave(g.nodes[2]), _ave(g.nodes[3]))
+end
+
+struct VectorField{T,U,V}
+    xgrid::Grid{T}
+    ygrid::Grid{T}
+    zgrid::Grid{T}
+    F::Array{U, 4}
+    xinterp::V
+    yinterp::V
+    zinterp::V
+    function VectorField(gx::Grid{T},gy,gz, F::AbstractArray{U,4}) where {T,U}
+        @views begin
+            xinterp = extrapolate(interpolate(xgrid.nodes, F[:,:,:,1], Gridded(Linear())), Flat())
+            yinterp = extrapolate(interpolate(xgrid.nodes, F[:,:,:,2], Gridded(Linear())), Flat())
+            zinterp = extrapolate(interpolate(xgrid.nodes, F[:,:,:,3], Gridded(Linear())), Flat())
+        end
+        V = typeof(xinterp)
+        new{T,U,V}(xgrid, ygrid, zgrid, F, xinterp, yinterp, zinterp)
+    end
+end
+
+function covariant(maingrid::Grid, field)
+    mx, my, mz = maingrid.nodes
+    dx, dy, dz = _dualgrid(maingrid)
+    VectorField(
+        Grid(mx, dy, dz),
+        Grid(dx, my, dz),
+        Grid(dx, dy, mz),
+        field
+    )
+end
+function contravariant(maingrid::Grid{T}, field) where T
+    mx, my, mz = maingrid.nodes
+    dx, dy, dz = _dualgrid(maingrid)
+    VectorField(
+        Grid(dx, my, mz),
+        Grid(mx, dy, mz),
+        Grid(mx, my, dz),
+        field
+    )
+end
+
+function (f::VectorField)(x)
+    SA[f.xinterp(x...), f.yinterp(x...), f.zinterp(x...)]
+end
+
 function loadvector(prefix, name, step=-1)
     h = hr(prefix, name)
     _, raw = h.get_timestep(step)
@@ -30,6 +95,19 @@ end
 
 # There was a problem writing bt data for 2020-Jan-23/pluto-2.
 # Timestep 27 is the last good step.
+function loadfields2(prefix, step=-1)
+    _, E_data = hr(prefix, "E").get_timestep(step)
+    _, B_data = hr(prefix, "bt").get_timestep(step)
+    E_data *= u"km/s^2"*(m_p/e)
+    B_data *= u"s^-1"*(m_p/e)
+    para = ParameterSet(prefix)
+    nodes = Tuple(convert(Array{Float64}, gp)*u"km" for gp in para.grid_points)
+    maingrid = Grid(nodes)
+    E = contravariant(maingrid, E_data)
+    B = covariant(maingrid, B_data)
+    return E,B
+end
+
 function loadfields(prefix, step=-1)
     E_data = loadvector(prefix, "E", step)u"km/s^2" * (m_p/e)
     B_data = loadvector(prefix, "bt", step)u"s^-1" * (m_p/e)
